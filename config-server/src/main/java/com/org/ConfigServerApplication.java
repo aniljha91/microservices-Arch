57ea549161d8dda0c496cc488d83bd9c9143093c
aniljha91 <aniljha_91@yahoo.co.in>
2017-06-07 12:42:26 +0530

    Committing Config-Server changes post AWS integration.

--- config-server/src/main/java/com/org/ConfigServerApplication.java
+++ config-server/src/main/java/com/org/ConfigServerApplication.java
@@ -2,13 +2,20 @@ package com.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConfigServerApplication.class);
 	}
 }
