e3581fb13772e0f005f5adb441dd4658439ba369
aniljha91 <aniljha_91@yahoo.co.in>
2017-06-08 12:36:58 +0530

    Committing hystrix-server changes post AWS integration.

--- hystrix-monitoring/src/main/java/com/org/HystrixMonitoringApplication.java
+++ hystrix-monitoring/src/main/java/com/org/HystrixMonitoringApplication.java
@@ -2,6 +2,8 @@ package com.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -10,9 +12,14 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableTurbine
-public class HystrixMonitoringApplication {
+public class HystrixMonitoringApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixMonitoringApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HystrixMonitoringApplication.class);
 	}
 }
