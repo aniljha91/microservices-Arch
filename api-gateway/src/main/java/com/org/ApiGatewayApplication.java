41320d4b060b580cc5451ca1a65739ddffe162a6
aniljha91 <aniljha_91@yahoo.co.in>
2017-06-27 15:32:40 +0530

    committing zuul changes

--- api-gateway/src/main/java/com/org/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/org/ApiGatewayApplication.java
@@ -3,12 +3,13 @@ package com.org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.ZuulProxyConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+@Import(value={ZuulProxyConfiguration.class})
 public class ApiGatewayApplication extends ResourceServerConfigurerAdapter{
 	
 	public static void main(String[] args) {
